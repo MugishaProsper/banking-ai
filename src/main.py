a0b636aa76996c1e04b12e4e220de35d6290e9c9
Polo <148433691+MugishaProsper@users.noreply.github.com>
2025-10-19 20:11:36 +0200

    minor additions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title = "Banking System AI Microservice",
+    version = "1.0.0"
+)
 
+app.add_middleware(
+    CORSMiddleware,
+    allowed_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
